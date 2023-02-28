660b589de19eeadd33da26414bec860ba21b6ef4
noori <rajilabana@gmail.com>
2023-02-28 21:36:30 +0000

    initial commit started prompt/file structure

--- starter/index.js
+++ starter/index.js
@@ -10,8 +10,108 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./src/page-template.js");
 
+const teamMembers = [];
+const idArray = [];
+
+function appMenu() {
+    function createManager() {
+      console.log("Please build your team");
+      inquirer.prompt([
+        {
+          type: "input",
+          name: "managerName",
+          message: "What is the team manager's name?",
+          validate: answer => {
+            if (answer !== "") {
+              return true;
+            }
+            return "Please enter at least one character.";
+          }
+        },
+        {
+          type: "input",
+          name: "managerId",
+          message: "What is the team manager's id?",
+          validate: answer => {
+            const pass = answer.match(
+              /^[1-9]\d*$/
+            );
+            if (pass) {
+              return true;
+            }
+            return "Please enter a positive number greater than zero.";
+          }
+        },
+        {
+          type: "input",
+          name: "managerEmail",
+          message: "What is the team manager's email?",
+          validate: answer => {
+            const pass = answer.match(
+              /\S+@\S+\.\S+/
+            );
+            if (pass) {
+              return true;
+            }
+            return "Please enter a valid email address.";
+          }
+        },
+        {
+          type: "input",
+          name: "managerOfficeNumber",
+          message: "What is the team manager's office number?",
+          validate: answer => {
+            const pass = answer.match(
+              /^[1-9]\d*$/
+            );
+            if (pass) {
+              return true;
+            }
+            return "Please enter a positive number greater than zero.";
+          }
+        }
+      ]).then(answers => {
+        const manager = new Manager(answers.managerName, answers.managerId, answers.managerEmail, answers.managerOfficeNumber);
+        teamMembers.push(manager);
+        idArray.push(answers.managerId);
+        createTeam();
+      });
+    }
+  // Function that cycle through the list of choices
+  function createTeam() {
+    inquirer.prompt([
+        {
+            type: "list",
+            name: "teamChoice",
+            message: "Which type of team member would you like to add?",
+            choices: [
+              "Engineer",
+              "Intern",
+              "I don't want to add any more team members"
+            ]   
+        }
+
+    ]).then(userChoice => {
+        switch (userChoice.teamChoice) {
+            case "Engineer":
+                addEngineer();
+                break;
+                case "Intern":
+                    addIntern();
+                    break;
+                    default:
+                        buildTeam();
+        }
+    })
+  }
+  
+  
+  
+  
+  
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
+
 inquirer.prompt([{
     //manager questions
 }]).then(response => {
@@ -21,6 +121,7 @@ inquirer.prompt([{
 
 const promptForNextEmployee = () => {
     inquirer.prompt([{
+        
         // choice of 3
     }]).then(response => {
         // if engineer
@@ -52,4 +153,5 @@ const promptForIntern = () => {
 
 const buildPage = () => {
 // render(myArrayOfTeamMembers)
+}
 }
